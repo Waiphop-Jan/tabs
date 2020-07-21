62696b4269c23303c07aa7c85c7c0fb907bb4e4d
Waiphop-Jan <wypopeza@gmail.com>
2020-07-21 10:27:16 +0700

    change-JS

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 
-const tabs = document.querySelectorAll('.tab')
+// const tabs = document.querySelectorAll('.tab')
+// const tabby = document.querySelector('.tabby-container')
+// const tabContents = document.querySelectorAll('.tab-content')
+
+// tabs.forEach(tab => {
+//     tab.addEventListener('click', event => {
+//         const target = tab.dataset.target
+//         const content = tabby.querySelector("#" + target)
+
+//         tabs.forEach(t => t.classList.remove('is-selected'))
+//         tab.classList.add('is-selected')
+
+//         tabContents.forEach(c => c.classList.remove('is-selected'))
+//         content.classList.add('is-selected')
+//     })
+// })
+
 const tabby = document.querySelector('.tabby-container')
-const tabContents = document.querySelectorAll('.tab-content')
+const tabsList = tabby.querySelector('.tabs-container')
+const tabsAll = Array.from(tabby.querySelectorAll('.tab'))
+const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', event => {
-        const target = tab.dataset.target
-        const content = tabby.querySelector("#" + target)
+tabsList.addEventListener('click', event => {
+    const tab = event.target
+    const target = tab.dataset.target
+    const tabContent = tabby.querySelector('#' + target)
 
-        tabs.forEach(t => t.classList.remove('is-selected'))
-        tab.classList.add('is-selected')
+    tabsAll.forEach(t => t.classList.remove('is-selected'))
+    tab.classList.add('is-selected')
 
-        tabContents.forEach(c => c.classList.remove('is-selected'))
-        content.classList.add('is-selected')
-    })
+    tabContents.forEach(c => c.classList.remove('is-selected'))
+    tabContent.classList.add('is-selected')
 })
